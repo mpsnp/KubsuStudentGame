73fcd6168cb1bc4c2cb1a8b9b1064429c052b009
Rashid Khuramshin <hrs.rashid@gmail.com>
2013-01-08 15:42:32 +0400

    map templates generator

---
+++ data/maps/gen_template.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print("Map name:")
+name = raw_input(">>")
+print("width:")
+width = input(">>")
+print("height")
+height = input(">>")
+
+with open(name, "wt") as fd :
+	fd.write(str(width)+" "+str(height)+"\n")
+	
+	for i in range(0, height) :
+		fd.write('1')
+	fd.write('\n')
+	
+	for i in range(2, width) :
+		fd.write('1')
+		for j in range(2, height) :
+			fd.write("0")
+		fd.write('1\n')
+	
+	for i in range(0, height) :
+		fd.write('1')
+	fd.write('\n')	
